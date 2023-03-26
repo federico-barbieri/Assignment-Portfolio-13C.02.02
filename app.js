fbaaa50602f8b0872e32de95fc86e517e824f460
Federico Barbieri <federicobarbieri54@gmail.com>
2023-03-26 15:36:18 +0200

    v7

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {timeline} from "https://cdn.skypack.dev/motion";
+import {timeline, inView} from "https://cdn.skypack.dev/motion";
 
 
 const sequence = [
@@ -9,6 +9,24 @@ const sequence = [
 
   timeline(sequence, {repeat: 1});
 
+  //
+  //
 
+  //h2
 
+  const assignmentH2 = document.querySelector('.assignment-h2')
+
+  const projectTitleH2 = document.querySelector('.project-title');
+
+  const finalThoughtsH2 = document.querySelector('.final-thoughts-h2');
+  
+  function iSeeYou(el){
+    el.classList.add('sup');
+}
+
+  inView(assignmentH2, iSeeYou(assignmentH2), {root: assignmentH2});
+
+  inView(projectTitleH2, iSeeYou(projectTitleH2), {root: projectTitleH2});
+
+  inView(finalThoughtsH2, iSeeYou(finalThoughtsH2), {root: finalThoughtsH2});
 
